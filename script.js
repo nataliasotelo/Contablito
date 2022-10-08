ddc1f5f6dddbe239b216277cd20c3cfb0678bc5f
nataliasotelo <110506914+nataliasotelo@users.noreply.github.com>
2022-10-08 14:15:15 -0300

    zz

---
+++ script.js
@@ -0,0 +1,10 @@
+ingresar.addEventListener("click", function (evt) {
+    let arrayFila = [document.]
+    document.getElementById("filas_tabla").innerHTML += `<tr>
+            <th scope="col">` + arrayFila[0] + `</th>
+            <th scope="col">` + arrayFila[1] + `</th>
+            <th scope="col">` + arrayFila[2] + `</th>
+            <th scope="col">` + arrayFila[3] + `</th>
+            <th scope="col">` + arrayFila[4] + `</th>
+        </tr>`
+})
